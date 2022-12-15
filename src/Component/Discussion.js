1a4dc0ee736b56005807c14b7d22a7b9c2e107e1
kimdonggu42 <kdonggoo44@gmail.com>
2022-12-15 20:07:57 +0900

    GET, POST, DELETE 기능 추가

--- src/Component/Discussion.js
+++ src/Component/Discussion.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Discussion.css';
 
-function Discussion({ list }) {
+function Discussion({ list, deleteButton }) {
 
     return (
         <li className="discussion__container">
+            <div className="discussion__answered">{list.answer !== null ? <i className="far fa-square-check"></i> : <i className="fa-regular fa-square"></i>}</div>
             <div className="discussion__avatar--wrapper">
                 <img className="discussion__avatar--image" src={list.avatarUrl} />
             </div>
@@ -12,7 +13,9 @@ function Discussion({ list }) {
                 <h2 className="discussion__title"><a href={list.url}>{list.title}</a></h2>
                 <div className="discussion__information">{list.author + `/` + new Date(list.createdAt).toLocaleString()}</div>
             </div>
-            <div className="discussion__answered">{list.answer !== null ? <i className="far fa-square-check"></i> : <i className="fa-regular fa-square"></i>}</div>
+            <div className="delete_Wrapper">
+                <button className='deleteButton' onClick={() => deleteButton(list.id)}>삭제</button>
+            </div>
         </li>
     );
 }
--- src/Pages/Discussions.js
+++ src/Pages/Discussions.js
@@ -2,38 +2,27 @@ import React, { useEffect, useState } from 'react';
 import './Discussions.css';
 import Discussion from '../Component/Discussion';
 
-
 function Discussions() {
     const [question, setQuestion] = useState([]);
     const [author, setAuthor] = useState('');
     const [title, setTitle] = useState('');
 
     useEffect(() => {
-    fetch('http://localhost:4000/discussions')
-        .then((response) => response.json())
-        .then((data) => {setQuestion(data)});
-    }, [])
+        getQuestion();
+      }, []);
 
-    // const handleButtonClick = () => {
-    //     const newObj = {
-    //         id: question.length + 1,
-    //         createdAt: new Date(),
-    //         author: author,
-    //         title: title,
-    //         url: "#",
-    //         answer: null,
-    //         avatarUrl: 'https://source.boringavatars.com/beam',
-    //     };
-    //     setQuestion([newObj, ...question])
-    // };
-
+    const getQuestion = () => {
+        fetch('http://localhost:4000/discussions')
+            .then((response) => response.json())
+            .then((data) => setQuestion(data));
+    }
 
     const handleButtonClick = () => {
-        fetch('http://localhost:4000/discussions', {
+        fetch('http://localhost:4000/discussions',{
             method: 'POST',
-            headers: { 'Content-Type': 'application/json; charset=utf-8' },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                id: question.length + 1,
+                id: question.length + 10,
                 createdAt: new Date(),
                 author: author,
                 title: title,
@@ -43,9 +32,15 @@ function Discussions() {
             }),
         })
             .then((response) => response.json())
-            .then((data) => { setQuestion([data, ...question]) });
+            .then((data) => getQuestion(data));
     }
-
+  
+    const handleDeleteClick = (id) => {
+        fetch(`http://localhost:4000/discussions/${id}`, {
+            method: 'DELETE'
+        })
+        .then((response) => { getQuestion(response) })
+    }
 
     const handleChangeAuthor = (event) => {
         setAuthor(event.target.value)
@@ -69,12 +64,12 @@ function Discussions() {
                     </div>
                 </div>
                 <div className="submit_Wrapper">
-                    <button className='submitButton' onClick={handleButtonClick}>SUBMIT</button>
+                    <button className='submitButton' onClick={handleButtonClick}>등록</button>
                 </div>
             </div>
             <ul className='discussion__wrapper'>
                 {question.map((value) =>
-                    <Discussion list={value} key={value.id} />
+                    <Discussion list={value} key={value.id} deleteButton={handleDeleteClick}/>
                 )}
             </ul>
         </React.Fragment>
@@ -82,3 +77,85 @@ function Discussions() {
 }
 
 export default Discussions;
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import './Discussions.css';
+// import Discussion from '../Component/Discussion';
+
+// function Discussions() {
+//     const [question, setQuestion] = useState([]);
+//     const [newQuestion, setNewQuestion] = useState([]);
+//     const [delQuestion, setDelQuesrion] = useState([]);
+//     const [author, setAuthor] = useState('');
+//     const [title, setTitle] = useState('');
+
+
+
+//     useEffect(() => {
+//         fetch('http://localhost:4000/discussions')
+//             .then((response) => response.json())
+//             .then((data) => setQuestion(data));
+//     }, [newQuestion])
+
+//     const handleButtonClick = () => {
+//         const newQuestion = {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 id: question.length + 10,
+//                 createdAt: new Date(),
+//                 author: author,
+//                 title: title,
+//                 url: "#",
+//                 answer: null,
+//                 avatarUrl: 'https://source.boringavatars.com/beam',
+//             }),
+//         };
+//         fetch('http://localhost:4000/discussions', newQuestion)
+//             .then((response) => response.json())
+//             .then((data) => setNewQuestion(data));
+//     }
+  
+//     const handleDeleteClick = (id) => {
+//         fetch(`http://localhost:4000/discussions/${id}`, {
+//             method: 'DELETE'
+//         })
+//     }
+
+//     const handleChangeAuthor = (event) => {
+//         setAuthor(event.target.value)
+//     };
+
+//     const handleChangeTitle = (event) => {
+//         setTitle(event.target.value)
+//     };
+
+//     return (
+//         <React.Fragment>
+//             <div className="input__container">
+//                 <div className="input__Wrapper">
+//                     <div className="input">
+//                         <label className="name">Name</label>
+//                         <input className="input__author" type="text" value={author} onChange={handleChangeAuthor}></input>
+//                         <label className="title">Title</label>
+//                         <input className="input__title" type="text" value={title} onChange={handleChangeTitle}></input>
+//                         <label className="question">Question</label>
+//                         <textarea className="input__question"></textarea>
+//                     </div>
+//                 </div>
+//                 <div className="submit_Wrapper">
+//                     <button className='submitButton' onClick={handleButtonClick}>SUBMIT</button>
+//                 </div>
+//             </div>
+//             <ul className='discussion__wrapper'>
+//                 {question.map((value) =>
+//                     <Discussion list={value} key={value.id} deleteButton={handleDeleteClick}/>
+//                 )}
+//             </ul>
+//         </React.Fragment>
+//     );
+// }
+
+// export default Discussions;
